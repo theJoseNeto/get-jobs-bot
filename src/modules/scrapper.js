f2fdc09be3707a5af4e42a49b63bcb83fa8de0cf
theJoseNeto <joseneto180201@gmail.com>
2022-06-27 11:02:43 -0300

    scrapper funcionando (versão protótipo)

--- src/modules/scrapper.js
+++ src/modules/scrapper.js
@@ -1,6 +1,48 @@
 const puppeteer = require("puppeteer");
 
-puppeteer.launch({headless: false}).then(() => {
-    console.log("browser is running! 🏆")
-});
+class Scrapper {
+    constructor() {
+        this.browser = null;
+        this.page = null;
+    }
 
+    scrapper = async () => {
+        await this.launchBrowser();
+        await this.gotoPage("https://www.linkedin.com/jobs/");
+        await this.searchJobs("Junior nodeJs");
+        await this.getListJobs();
+
+    }
+
+    launchBrowser = async () => {
+        this.browser = await puppeteer.launch({
+            headless: true
+        });
+    }
+
+    gotoPage = async url => {
+        this.page = await this.browser.newPage();
+        await this.page.goto(url);
+    }
+
+    searchJobs = async searchThis => {
+        await this.page.waitForSelector('.dismissable-input__input')
+        await this.page.type('.dismissable-input__input', searchThis, {
+            delay: 200
+        });
+        await this.page.keyboard.press("Enter");
+    }
+
+    getListJobs = async () => {
+        return new Promise(async (resolve, reject) => {
+            await this.page.waitForSelector(".base-card__full-link");
+            await this.page.$eval(".base-card__full-link", elem => elem.href)
+            .then((hrefValue)=> {
+                console.log(hrefValue)
+            });
+        })
+    }
+}
+const scrp = new Scrapper();
+
+scrp.scrapper();
