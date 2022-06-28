42fb68b0abc1763cdc1f233aa3719a0bc91c3485
theJoseNeto <joseneto180201@gmail.com>
2022-06-28 13:12:05 -0300

    classe 'Scrapper' finalizada. Obtenção de lista links para vagas funcionando

--- src/modules/scrapper.js
+++ src/modules/scrapper.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 
 class Scrapper {
+
     constructor() {
         this.browser = null;
         this.page = null;
@@ -10,8 +11,9 @@ class Scrapper {
         await this.launchBrowser();
         await this.gotoPage("https://www.linkedin.com/jobs/");
         await this.searchJobs("Junior nodeJs");
-        await this.getListJobs();
+        const jobs = await this.getListJobs().finally(() => this.browser.close());
 
+        return Promise.resolve(jobs);
     }
 
     launchBrowser = async () => {
@@ -36,13 +38,17 @@ class Scrapper {
     getListJobs = async () => {
         return new Promise(async (resolve, reject) => {
             await this.page.waitForSelector(".base-card__full-link");
-            await this.page.$eval(".base-card__full-link", elem => elem.href)
-            .then((hrefValue)=> {
-                console.log(hrefValue)
+            const links = await this.page.$$(".base-card__full-link");
+            const jobs = [];
+            links.forEach(async (link, index) => {
+                link = await (await link.getProperty("href")).jsonValue();
+                jobs.push(link);
+                if(index === links.length -1) {
+                    resolve(jobs);
+                }
             });
-        })
+        });
     }
 }
-const scrp = new Scrapper();
 
-scrp.scrapper();
+module.exports = Scrapper;
