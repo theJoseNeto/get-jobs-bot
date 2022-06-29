0ad91f387ef0fa230c2d1ec7c9c7f91dc54e478a
theJoseNeto <joseneto180201@gmail.com>
2022-06-29 17:08:40 -0300

    dependencia necessária para o bot instalada e funcionalidade de modificar a localidade das vagas adicionada

--- src/modules/scrapper.js
+++ src/modules/scrapper.js
@@ -7,18 +7,23 @@ class Scrapper {
         this.page = null;
     }
 
-    scrapper = async () => {
-        await this.launchBrowser();
-        await this.gotoPage("https://www.linkedin.com/jobs/");
-        await this.searchJobs("Junior nodeJs");
-        const jobs = await this.getListJobs().finally(() => this.browser.close());
+    scrapper = async (job, locale) => {
 
-        return Promise.resolve(jobs);
+        return new Promise(async (resolve, reject)=>{
+            await this.launchBrowser();
+            await this.gotoPage("https://www.linkedin.com/jobs/");
+            await this.searchJobs(job, locale);
+            await this.getListJobs()
+            .then(results => resolve(results))
+            .finally(() => this.browser.close());
+        });
+
     }
 
-    launchBrowser = async () => {
+    launchBrowser = async (headless) => {
+
         this.browser = await puppeteer.launch({
-            headless: true
+            headless: headless,
         });
     }
 
@@ -27,19 +32,24 @@ class Scrapper {
         await this.page.goto(url);
     }
 
-    searchJobs = async searchThis => {
-        await this.page.waitForSelector('.dismissable-input__input')
-        await this.page.type('.dismissable-input__input', searchThis, {
-            delay: 200
-        });
+    searchJobs = async (job, locale) => {
+        await this.page.waitForSelector('.dismissable-input__input');
+        //job
+        await this.page.type('.dismissable-input__input', job, {delay: 200});
+        // cleanup locale input value
+        await this.page.evaluate(()=> document.querySelector('input[name="location"]').value = "");
+        // new locale input value
+        await this.page.type('input[name="location"]', locale, {delay: 200});
         await this.page.keyboard.press("Enter");
     }
 
     getListJobs = async () => {
         return new Promise(async (resolve, reject) => {
             await this.page.waitForSelector(".base-card__full-link");
+
             const links = await this.page.$$(".base-card__full-link");
             const jobs = [];
+            
             links.forEach(async (link, index) => {
                 link = await (await link.getProperty("href")).jsonValue();
                 jobs.push(link);
@@ -48,7 +58,12 @@ class Scrapper {
                 }
             });
         });
+    
     }
+
+
 }
+const scrp = new Scrapper();
+
 
 module.exports = Scrapper;
