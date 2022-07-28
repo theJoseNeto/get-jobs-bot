34fc0247879190f6ee04bae3737216a87157a2fb
= <=>
2022-07-28 07:18:19 -0300

    express removed

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const token = process.env.TOKEN;
 
 const { botActions } = require('./src/modules/discord-bot');
 
-app.get('/', (req, res) => {
-    res.send("<p>Essa aplicação só pode ser acessada no nosso <a href='https://discord.gg/wRu4bewKFD'>discord</a></p> <br/> <p><a href='https://minhastack.slcn.app'>MinhaStack</a></p>")
-});
-
-
     client.login(token).then(() => {
         client.on("messageCreate", (message) => botActions(client, message));
         client.on("ready", () => console.log('client is ready!'));
