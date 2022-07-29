170b11ffa634279c6263af02c75567b5d70feac3
= <=>
2022-07-29 14:41:25 -0300

    dotenv adicionado

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const { Client, Intents } = require("discord.js");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
